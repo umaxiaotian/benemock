f2ba638b48b08b45d31322d0cbb61195d20a8ec4
umaxiaotian <yuma@umaxiaotian.com>
2023-07-02 01:06:55 +0900

    初期コミット

---
+++ Ansible/app/main.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+# CORSを回避するために追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
